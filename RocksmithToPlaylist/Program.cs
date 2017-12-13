4c7602f76278230d359eedb3d6951feb535c9d8b
Roy W. Gero II <roy.gero.ii@gmail.com>
2017-12-13 09:33:23 -0500

    Initial Commit of the Project as I've been playing around with it.

---
+++ RocksmithToPlaylist/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace RocksmithToPlaylist
+{
+    class Program
+    {
+
+        static List<SongInfo> masterSongList = new List<SongInfo>();
+
+        static List<string> scanDirectory(string fileExt, string path)
+        {
+            string ext = "*_p." + fileExt;
+            return Directory.GetFiles(path, ext, SearchOption.AllDirectories).ToList<string>();
+        }
+
+        static void Main(string[] args)
+        {
+
+            /* Here is the intended workflow
+                    
+                - Get list of files
+                - Parse each file
+                - Write to CSV file.
+
+            */
+
+
+            var dlcDirectory = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Rocksmith2014\\dlc\\";
+            List<string> files = scanDirectory("psarc", dlcDirectory);
+
+            files.Add("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Rocksmith2014\\songs.psarc");
+
+            //Writing to file temporarily
+            foreach (string i in files)
+            {
+                var browser = new PsarcBrowser(i);
+                var songList = browser.GetSongList();
+                foreach (var song in songList)
+                {
+                    masterSongList.Add(song);
+                }
+
+            }
+            using (System.IO.StreamWriter file =
+                new System.IO.StreamWriter(@"C:\\Test\\Test.txt"))
+            {
+                foreach (SongInfo song in masterSongList)
+                {
+                    string line = String.Format("{0}\t{1}\t{2}\t{3}",
+                            song.Artist, song.Title, song.Album, song.Year,
+                            string.Join(", ", song.Arrangements));
+
+                    file.WriteLine(line);
+                }
+            }
+        }
+    }
+}
